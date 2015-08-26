29c2b772071c38c2cc962e0e4cd8dd8c2918745e
tjsiwinski <tjsiwinski@users.noreply.github.com>
2015-08-26 16:43:12 -0500

    Update adc-test.ps1

--- adc-test.ps1
+++ adc-test.ps1
@@ -1,4 +1,15 @@
 $sMyLog=$MyInvocation.MyCommand.Definition +".txt"
 Add-Content $sMyLog "$(Get-Date) - *** Script start *** "
-Write-Host "Hello World"
+Add-Content $sMyLog "Hello World"
+$InDevice= (Get-Content .\Output.json) -join "`n" | ConvertFrom-Json
+Add-Content $sMyLog "Account: $($InDevice.Account)"
+Add-Content $sMyLog "AccountName: $($InDevice.'Account Name')"
+Add-Content $sMyLog "DC: $($InDevice.Datacenter)"
+Add-Content $sMyLog "Name: $($InDevice.Name)"
+Add-Content $sMyLog "NickName: $($InDevice.NickName)"
+Add-Content $sMyLog "Number: $($InDevice.Number)"
+Add-Content $sMyLog "OSName: $($InDevice.OSname)"
+Add-Content $sMyLog "OStype: $($InDevice.OStype)"
+Add-Content $sMyLog "Platform: $($InDevice.PlatName)"
 Add-Content $sMyLog "$(Get-Date) - *** Script end *** "
+
