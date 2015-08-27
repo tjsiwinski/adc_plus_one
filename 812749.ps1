e236d1d26f3f5952b7a5a64b3d095f0e589621f2
tjsiwinski2000 <tjsiwinski_2000@yahoo.com>
2015-08-27 14:27:45 -0500

    Update in Testing
    8/27/15 log to ticket and output with in flags

--- 812749.ps1
+++ 812749.ps1
@@ -1,14 +1,25 @@
 $sMyLog=$MyInvocation.MyCommand.Definition +".txt"
-Add-Content $sMyLog "$(Get-Date) - *** Script start *** "
-Add-Content $sMyLog "Hello World"
-$InDevice= (Get-Content c:\rs-pkgs\ice\output.json) -join "`n" | ConvertFrom-Json
-Add-Content $sMyLog "Account: $($InDevice.Account)"
-Add-Content $sMyLog "AccountName: $($InDevice.'Account Name')"
-Add-Content $sMyLog "DC: $($InDevice.Datacenter)"
-Add-Content $sMyLog "Name: $($InDevice.Name)"
-Add-Content $sMyLog "NickName: $($InDevice.NickName)"
-Add-Content $sMyLog "Number: $($InDevice.Number)"
-Add-Content $sMyLog "OSName: $($InDevice.OSname)"
-Add-Content $sMyLog "OStype: $($InDevice.OStype)"
-Add-Content $sMyLog "Platform: $($InDevice.PlatName)"
-Add-Content $sMyLog "$(Get-Date) - *** Script end *** "
+$sOut="`r`n$($(Get-Date)) - *** Script start *** "
+
+$InDevice= (Get-Content c:\rs-pkgs\ice\output.json) -join "`r`n" | ConvertFrom-Json
+$sOut +="`r`n AccountNum: $($InDevice.Account)" 
+
+$sOut +="`r`n AccountName:  AccountName: $($InDevice.'Account Name')"
+$sOut +="`r`n DC:  $($InDevice.Datacenter)"
+$sOut +="`r`n DeviceName:  $($InDevice.Name)"
+$sOut +="`r`n DeviceNickName:  $($InDevice.NickName)"
+$sOut +="`r`n DeviceNum:  $($InDevice.Number)"
+$sOut +="`r`n OSName: $($InDevice.OSname)"
+$sOut +="`r`n OStype: $($InDevice.OStype)"
+$sOut +="`r`n Platform: $($InDevice.PlatName)"
+$sOut +="`r`n$($(Get-Date)) - *** Script end *** "
+Add-Content $sMyLog $sOut 
+
+Write-Host RBA START STATUS:
+Write-Host Success
+Write-Host RBA END STATUS:
+Write-Host .
+Write-Host RBA START DATA:
+Write-Host $sOut
+Write-Host
+Write-Host RBA END DATA:
