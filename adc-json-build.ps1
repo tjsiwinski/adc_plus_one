7e093377884a5acf1b8564c3e63437414de1ed49
tjsiwinski2000 <tjsiwinski_2000@yahoo.com>
2015-09-30 14:54:36 -0500

    Test 9/30/2015
    Git Client Test after issues noted this week by JT and John Luikart

---
+++ adc-json-build.ps1
@@ -0,0 +1,11 @@
+﻿##sample JSON param { "Name": "429362-wincline.local",  
+#NOTE:  `{`"Name`":`"429362-wincline.local`"`} returns {"Name":"429362-wincline.local"}
+#        "`{`"Name`":`"429362 -wincline .local`"`} " *** start & end with " , escape {,}," with `
+#         """{`"Name`":`"429362-wincline.  local`"}""" start & end with """  and escape " within the body within `
+
+param($sIn)
+# $sIn starts and ends with an extraneous double quote that  needs to be stripped.
+
+$sMyLog=$MyInvocation.MyCommand.Definition +".txt"
+# $sIn starts and ends with an extraneous double quote that  needs to be stripped.
+Add-Content $sMyLog $sIn.Substring(1,$sIn.Length-2)
